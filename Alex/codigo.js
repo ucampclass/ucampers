dd66a8d3dc89c177a379e17fc4c852ae39fd22ef
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2021-10-21 23:19:10 -0500

    Ejercicio 2 para mostrar mensaje de alerta con uso de una variable

--- Alex/codigo.js
+++ Alex/codigo.js
@@ -3,3 +3,6 @@ console.log("hola mundo");
 //console.log funciona para indicar en la consola
 //en la línea 5, que es el primer script
 console.log("Soy el primer script");
+
+var mensaje = "Hola mundo!\n qué facil es incluir 'comillas' simples\n y \"comillas dobles\" " ;
+alert(mensaje);
