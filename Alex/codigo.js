62d310709f302d52365da5f97c41aeae7c637f7d
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2021-10-21 23:55:28 -0500

    Ejercicio 3

--- Alex/codigo.js
+++ Alex/codigo.js
@@ -6,3 +6,24 @@ console.log("Soy el primer script");
 
 var mensaje = "Hola mundo!\n qué facil es incluir 'comillas' simples\n y \"comillas dobles\" " ;
 alert(mensaje);
+/*
+Ejercicio 3
+*/
+var meses = ["Enero", "Febrero", "Marzo", "Abril", 
+                "Mayo", "Junio","Julio","Agosto",
+                "Septiembre","Octubre", "Noviembre", "Diciembre"];
+console.log(meses[0]);
+console.log(meses[1]);
+console.log(meses[2]);
+console.log(meses[3]);
+console.log(meses[4]);
+console.log(meses[5]);
+console.log(meses[6]);
+console.log(meses[7]);
+console.log(meses[8]);
+console.log(meses[9]);
+console.log(meses[10]);
+console.log(meses[11]);
+
+var meses2=[];
+alert(typeof meses2);
