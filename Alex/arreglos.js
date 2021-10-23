3461edb0e37ae868a4a2345c1ce82f2b098c28fd
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2021-10-23 10:16:33 -0500

    Agregar elementos al arreglo

--- Alex/arreglos.js
+++ Alex/arreglos.js
@@ -1,11 +1,17 @@
 const arreglo = ['Daniel','Juan','Pepe',15,true];
-//var nombre=new Array('Daniel','Juan','Pepe');
-/*var nombre1="Daniel";
+/*var nombre=new Array('Daniel','Juan','Pepe');
+var nombre1="Daniel";
 var nombre2="Juan";
 var nombre3="Pepe";
 
 console.log(nombre1);
 console.log(nombre2);
-console.log(nombre3);*/
+console.log(nombre3);
 arreglo[0]='Luis';
+console.log(arreglo);*/
+arreglo.push('Luis');
+arreglo.push ('Toño');
+arreglo.push('Juan');
+arreglo.push(5);
+
 console.log(arreglo);
