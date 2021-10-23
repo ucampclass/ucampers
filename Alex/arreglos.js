9d1915e1c983ff7775a23d5d0cfc8144053acbaa
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2021-10-23 12:19:58 -0500

    encontrar valores con la coincidencia del filtrado, la condición

--- Alex/arreglos.js
+++ Alex/arreglos.js
@@ -1,4 +1,4 @@
-const valores = ['Daniel', 'Juan', 'Juan','Pedro','daniel'];
+const valores = ['Daniel', 'Juan', 'Juan','Pedro','daniel','Juan Pablo','Juan de Dios'];
 
-const resultado = valores.filter((nombre)=>nombre ==='Juan');
+const resultado = valores.filter((nombre)=>nombre.includes('Juan'));
 console.log(resultado);
