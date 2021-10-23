c4f38209287029ff90cf8bb306c69760299e388f
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2021-10-23 16:17:16 -0500

    Resolución de ejercicio 1

--- Alex/arreglos.js
+++ Alex/arreglos.js
@@ -1,4 +1,3 @@
-//const valores = [1,2,3,4,5,6,7,8,9,10];
 //let edad = 30
 //concatenar un string
 //let nombre =`Alex mi edad es ${edad} años`;
@@ -9,6 +8,20 @@ for (let i=0; i<=10; i++){
     console.log(2*i);
 }*/
 
+/*segunda propuesta
 for (let i=1; i<=10; i++){
     console.log(`2 x ${i} = ${2*i}`);
+}*/
+
+/*tercera propuesta
+const tabla = [1,2,3,4,5,6,7,8,9,10];
+
+for (let i = 1; i<= tabla.length; i++) {
+    console.log(`2 x ${i} = ${2 * i}`);
+}*/
+
+//cuarta propuesta ejercicio 1
+const valores = [1,2,3,4,5,6,7,8,9,10];
+for (let i=1; i <=10; i++) {
+    console.log(`${i} x 2 = ${i*2}`);
 }
