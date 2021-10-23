83574a2913156448eb62501c2fcfd17639cfd531
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2021-10-23 10:43:47 -0500

    función de iterar

--- Alex/arreglos.js
+++ Alex/arreglos.js
@@ -1,23 +1,5 @@
 const arreglo = ['Daniel','Juan','Pepe',15,true];
-/*var nombre=new Array('Daniel','Juan','Pepe');
-var nombre1="Daniel";
-var nombre2="Juan";
-var nombre3="Pepe";
 
-console.log(nombre1);
-console.log(nombre2);
-console.log(nombre3);
-arreglo[0]='Luis';
-console.log(arreglo);
-arreglo.push('Luis');
-arreglo.push ('Toño');
-arreglo.push('Juan');
-arreglo.push(5);
-arreglo.unshift('Toño');
-arreglo.shift('Daniel');
-arreglo.splice(2,1);
-arreglo.splice(3,0,'Toño');
-console.log(arreglo);*/
-console.log(arreglo.length);
-arreglo.push('Luis');
-console.log(arreglo);
+for(let i=0; i <=6; i++) {
+    console.log(i);
+}
