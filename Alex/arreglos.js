211ebc68790c91869b2f44e55b9bd5bbfc3ebc2c
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2021-10-23 11:59:02 -0500

    reportar con función flecha

--- Alex/arreglos.js
+++ Alex/arreglos.js
@@ -1,5 +1,41 @@
 const arreglo = ['Daniel','Juan','Pepe',15,true,'Toño'];
 
-for(let i=0; i <6; i++) {
-    console.log(i);
+/*let indice =1;
+
+if(indice <2){
+
+    console.log('');
+
+    indice = indice +1;
 }
+
+for(let i=0; i <6; i++) {
+    console.log(arreglo[indice]);
+    console.log(indice);
+}
+
+for(let i=0; i <3; i++) {
+    console.log(casa[indice]);
+}
+
+console.log(arreglo,length);
+console.log(arreglo);*/
+
+function saluda(nombre, numero) {
+    console.log('Hola mundo' + nombre);
+    console.log('Estoy llamando a mi función 45');
+}
+
+let meLlamo = (nombre) =>{
+    alert('Mi nombre es'+nombre);
+};
+
+/*function sumarNumeros (valor1, valor2) {
+    return valor1 + valor2;
+}
+
+console.log(sumarNumeros(2, 5));*/
+
+arreglo.forEach(function (valores) {
+    console.log(valores);
+});
