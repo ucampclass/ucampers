eb7575553ae1b02c81afd4e4b47c093c0180928e
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2021-10-23 13:23:24 -0500

    propuesta para concatenar

--- Alex/arreglos.js
+++ Alex/arreglos.js
@@ -1,13 +1,6 @@
-/*const valores = [1,2,3,4,5,6,7,8,9,10];
-console.log(valores)
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}*/
+const valores = [1,2,3,4,5,6,7,8,9,10];
 
-const nombres=['alberto', 'paty', 'Jose', 'daniel', 'luis', 'antonio', 'Luis', 'paty', 'luis
-const nombreMayusculas = [];
-
-nombres.toUppercase
-    
-});
+//let edad = 30
+//concatenar un string
+//let nombre =`Alex mi edad es ${edad} años`;
+//console.log(nombre);
