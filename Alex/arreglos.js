3a2ba5453dca575d43368f1132aac91e36518272
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2021-10-23 12:32:09 -0500

    valor de actividad para acumulador

--- Alex/arreglos.js
+++ Alex/arreglos.js
@@ -1,4 +1,14 @@
-const valores = ['Daniel', 'Juan', 'Juan','Pedro','daniel','Juan Pablo','Juan de Dios'];
+const valores = [1,2,3,4,5];
 
-const resultado = valores.filter((nombre)=>nombre.includes('Juan'));
-console.log(resultado);
+const resultado =valores.reduce(
+    (acc,elem) =>(acc =acc+elem)
+);
+console.log(resultado)
+
+
+//const resultadoFilter =valores.filter((nombre)=> nombre==='Juan');
+//const resultadoFind =valores.find((nombre)=> nombre===nombre);
+
+//const resultado = valores.filter((nombre)=>nombre.==='Juan');
+//console.log(resultado);
+
