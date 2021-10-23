ece6a5e3405e4d74750345f15543a4f7a184deff
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2021-10-23 13:27:10 -0500

    dos propuestas

--- Alex/arreglos.js
+++ Alex/arreglos.js
@@ -1,6 +1,14 @@
-const valores = [1,2,3,4,5,6,7,8,9,10];
-
+//const valores = [1,2,3,4,5,6,7,8,9,10];
 //let edad = 30
 //concatenar un string
 //let nombre =`Alex mi edad es ${edad} años`;
 //console.log(nombre);
+
+/*primera propuesta
+for (let i=0; i<=10; i++){
+    console.log(2*i);
+}*/
+
+for (let i=1; i<=10; i++){
+    console.log(`2 x ${i} = ${2*i}`);
+}
