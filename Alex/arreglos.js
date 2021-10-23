41c4f236e402bfe81a4ce2517e8476b47e027ff8
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2021-10-23 10:09:58 -0500

    editar variable del arreglo

--- Alex/arreglos.js
+++ Alex/arreglos.js
@@ -1,7 +1,11 @@
-var nombre1="Daniel";
+const arreglo = ['Daniel','Juan','Pepe',15,true];
+//var nombre=new Array('Daniel','Juan','Pepe');
+/*var nombre1="Daniel";
 var nombre2="Juan";
 var nombre3="Pepe";
 
 console.log(nombre1);
 console.log(nombre2);
-console.log(nombre3);
+console.log(nombre3);*/
+arreglo[0]='Luis';
+console.log(arreglo);
