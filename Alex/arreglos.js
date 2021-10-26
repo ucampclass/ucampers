0959a6132856888d86a9eb48911c95616a5bbe4a
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2021-10-26 10:04:03 -0500

    Función map para obtener un cuatrimestre con inflación no mayor al 5%

--- Alex/arreglos.js
+++ Alex/arreglos.js
@@ -1,36 +1,3 @@
- const nombres = ['alberto', 'paty', 'Jose', 'daniel', 'luis', 'antonio', 'Luis', 'paty', 'luis'];
- const nombreMayusculas = nombres.map((nombres)=> nombres.toUpperCase());
-console.log(nombreMayusculas);
-
- let nombre = 'mayúsculas';
- console.log(nombre.toUpperCase());
-
-console.log('Valores del arreglo original:')
-nombres.forEach((nom)=>{
-    console.log(nom);
- });
-
-
- //declarar la función
- // function nombredelafuncion(){}
-
- 
-//function incorpora(valor, sintaxis){
-//console.log('Hola mundo!' + valor)
-//console.log('Estoy incorporando valores' + sintaxis); 
-// }
-
-// incorpora(' miren',' en orden');
-// incorpora(' observen', ' sin orden');
-// incorpora(' caigan en ello', ' aleatorio');
-
-//function sumarNumeros (valor1, valor2){
-//    return valor1 + valor2;
-//}
-//console.log(sumarNumeros(2,5));
-
-//const sumarNumeros = (valor1, valor2)=> {
-//    return valor1 + valor2;
-//}
-
-//console.log(sumarNumeros(1,5));
+const nombres = [3399,3399, 3399,3399];
+const nuevosValores = nombres.map ( (nom) => nom * 1.05);
+console.log (nuevosValores);
