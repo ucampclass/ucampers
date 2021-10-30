59d3834672cb56197831145c3a82baf28a53ca9f
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2021-10-30 09:43:08 -0500

    propiedad de objetos

--- Alex/arreglos.js
+++ Alex/arreglos.js
@@ -1,12 +1,15 @@
-let country = {
-    name: "México",
-    language: "Español",
-    getPopulation: function(){
-      return `En ${this.name}, se hablan más de 67 lenguas además del ${this.language}`
-    }
-  }
-  
- console.log(country)
- console.log(country.name)
- console.log(country.language)
- console.log(country.getPopulation())
+let nombreObjeto = new Object()
+
+nombreObjeto.nombre = "Joe"
+nombreObjeto.apellido = "Doe"
+nombreObjeto.edad = 35
+
+nombreObjeto.saludar = function(){
+  return `Hola. Un gusto.`
+}
+
+console.log(nombreObjeto.nombre);
+console.log(nombreObjeto.edad);
+
+console.log(nombreObjeto.apellido);
+console.log(nombreObjeto.saludar());
