741398ccb8875355e18471505e700c47a79e5adb
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2021-11-01 22:31:07 -0600

    getElementsById

--- Alex/index.js
+++ Alex/index.js
@@ -1,39 +1,5 @@
-class Persona {
-    nombre;
-    edad;
-    sexo;
-    saludar (){
-        console.log("Hola mi nombre es ${this.nombre}");
-    }
-}
+// Seleccionamos el elemento a través de su ID
+const main = document.getElementById("main")
 
-class Alumno extends Persona {
-    
-    matricula;
-    turno;
-    cicloEscolar;
-}
-
-class Profesor extends Persona {
-    materia;
-    turno;
-    cicloEscolar;
-}
-
-const alumnoJuan = new Alumno();
-alumnoJuan.nombre = 'Juan';
-alumnoJuan.edad = '18';
-alumnoJuan.sexo = 'Masculino';
-alumnoJuan.matricula = 'abc123';
-alumnoJuan.turno = 'Matutino';
-alumnoJuan.cicloEscolar = '2020-2021';
-alumnoJuan.saludar();
-
-const profesorPedro = new Profesor();
-profesorPedro.nombre = 'Pedro';
-profesorPedro.edad = '35';
-profesorPedro.sexo = 'M';
-profesorPedro.materia = 'Matematicas';
-profesorPedro.turno = 'Matutino';
-profesorPedro.cicloEscolar = '2020-2021';
-profesorPedro.saludar();
+// Verificamos en consola que obtenemos la etiqueta
+console.log(main)
