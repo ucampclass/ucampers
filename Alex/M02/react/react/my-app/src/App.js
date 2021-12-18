d607e9f842ae4799fe5464415200980f10a5e81a
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2021-12-17 20:54:22 -0600

    Con uso de Fragment, para evitar un div

--- Alex/M02/react/react/my-app/src/App.js
+++ Alex/M02/react/react/my-app/src/App.js
@@ -1,12 +1,15 @@
 import Saludar from "./components/Saludar";
 import Texto from "./components/Texto";
 import Imagen from "./components/Imagen";
+import Parrafo from "./components/Parrafo";
+
 function App() {
   return <div>
     <Saludar />
     <Texto />
     <Imagen />
     <Imagen />
+    <Parrafo />
     </div>;
 }
 
---
+++ Alex/M02/react/react/my-app/src/components/Parrafo.jsx
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react'
+
+export default function Parrafo() {
+    return (
+        <Fragment>
+            <h1>Este es un componente párrafo</h1>
+            <hr />
+            <p>Soy un párrafo</p>
+        </Fragment>
+    );
+};
