c63083e70ca4a3d2b6c3b38af48e01aef9e7404c
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2021-12-17 21:00:13 -0600

    sin Uso de div ni de fragment, para incluir varios elementos

--- Alex/M02/react/react/my-app/src/components/Parrafo.jsx
+++ Alex/M02/react/react/my-app/src/components/Parrafo.jsx
@@ -1,11 +1,11 @@
-import React, {Fragment} from 'react'
+import React from 'react'
 
 export default function Parrafo() {
     return (
-        <Fragment>
+        <>
             <h1>Este es un componente párrafo</h1>
             <hr />
             <p>Soy un párrafo</p>
-        </Fragment>
+        </>
     );
 };
