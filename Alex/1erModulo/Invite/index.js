24be9d5b7872b68a86f99e53ee0a3ebe98d7b634
bout14599 <joflores.c03@ucamp.utel.edu.mx>
2022-05-10 23:54:03 -0500

    Invitación al live de U Camp

---
+++ Alex/1erModulo/Invite/index.js
@@ -0,0 +1,9 @@
+const img = document.querySelector('img');
+img.setAttribute('src', 'https://media-exp1.licdn.com/dms/image/C5622AQH_GFzZVU416A/feedshare-shrink_800/0/1652124469168?e=1655337600&v=beta&t=Dzyjq1ey-xuNBfHvKsm3qiGbZInbURLNBzAzEV3mnoc');
+img.setAttribute('alt', 'Invite');
+img.classList.add('invite');
+
+const link = document.querySelector('a');
+link.setAttribute('href', 'https://www.instagram.com/ucampaprende/');
+link.setAttribute('target', '_blank');
+link.classList.add('link');
